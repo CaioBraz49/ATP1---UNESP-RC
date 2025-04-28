d0fea7e3838a35826716a074e44554062dc5c4e5
CaioBraz49 <124938346+CaioBraz49@users.noreply.github.com>
2025-04-28 16:25:20 -0300

    Cria 1.cpp na lista de exercícios 5

---
+++ Lista_de_exercicios_5/1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprime_ola() { // imprime "Ola, mundo!"
+    printf("Ola, mundo!\n");
+}
+
+int main() {
+    imprime_ola();  // chama a funcao
+    getchar();
+    return(0);
+}
